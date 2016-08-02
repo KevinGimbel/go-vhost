5436794ba8bc634d12eaad649c13f95ddb416ada
Kevin Gimbel <hallo@kevingimbel.com>
2016-08-02 11:53:26 +0200

    [WIP] Switch from pointer to regular strings (API/CLI)

--- vhost-cli.go
+++ vhost-cli.go
@@ -5,17 +5,30 @@ import (
 	vhost "github.com/kevingimbel/vhost/api"
 )
 
-var name = flag.String("name", "test", "Server Name for the new Virtual Host")
-var tld = flag.String("tld", "local", "TLD (Top Level Domain) to use")
-var port = flag.String("port", "80", "Defines the port to serve to")
-var docroot = flag.String("docroot", "/var/www/html/", "Document Root where the files live")
-var output = flag.String("output", "./", "Where to save the file")
-var template = flag.String("template", "./templates/apache.template", "Choose which template to load. Pass full path to template")
+var (
+	name     string
+	tld      string
+	port     string
+	docroot  string
+	output   string
+	template string
+)
+
+func initFlags() {
+	flag.StringVar(&name, "name", "test", "Server Name for the new Virtual Host")
+	flag.StringVar(&tld, "tld", "local", "TLD (Top Level Domain) to use")
+	flag.StringVar(&port, "port", "80", "Defines the port to serve to")
+	flag.StringVar(&docroot, "docroot", "/var/www/html/", "Document Root where the files live")
+	flag.StringVar(&output, "output", "./", "Where to save the file")
+	flag.StringVar(&template, "template", "./templates/apache.template", "Choose which template to load. Pass full path to template")
+
+	flag.Parse()
+}
 
 // Reads the command line flags and creates a
 // Vhost configuration.
 func assignVhostArguments() *vhost.Vhost {
-	flag.Parse()
+	initFlags()
 
 	hostConfig := new(vhost.Vhost)
 
